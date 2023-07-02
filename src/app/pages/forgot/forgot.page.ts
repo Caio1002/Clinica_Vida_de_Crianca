608ae1be71cc8d3940be1991c10eda97a0ca9f8a
CaioLDS <caiodaluz2002@gmail.com>
2023-07-02 20:27:01 -0300

    estilização final

--- src/app/pages/forgot/forgot.page.ts
+++ src/app/pages/forgot/forgot.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-forgot',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private nav: NavController
+  ) { }
 
   ngOnInit() {
+  }
+
+  move(toPage: string): void {
+    this.nav.navigateForward(toPage);
   }
 
 }
